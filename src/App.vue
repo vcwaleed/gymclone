0012b5acfeecbb5f5de6f451b782f935dc23e283
Talha Waleed <talhawaleed460@gmail.com>
2025-03-12 03:25:36 -0700

    start the project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+ 
+<h1>Hi </h1>
+</template>
+
+<style lang="scss" scoped>
+@use "../src/styles/variables" as *;
+@use "../src/styles/mixins" as * ;
+h1{
+  background-color:$secondary-color;
+}
+</style>
